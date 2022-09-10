6623ffe02f553315d9af2f49ec12e42dcc580159
ollie <olivinanzali07@gmail.com>
2022-09-09 20:03:43 -0400

    updated js

--- app.js
+++ app.js
@@ -2,26 +2,42 @@ const express = require('express')
 const app = express();
 const ExpressError = require('./expressError')
 
-app.get('/mean', (req, res, next) => {
-  let result = {
-    operation: "mean",
-    result: meanFinder(nums)
+app.post('/mean', (req, res, next) => {
+    // list of numbers
+  let numbers = req.body['nums']
+  let arrayOfNumbers = numbers.split(',');
+  let sumOfNumbers = 0;
+  for(let i =0; i < arrayOfNumbers.length; i++){
+    let currentNumber = arrayOfNumbers[i]
+    sumOfNumbers = sumOfNumbers + currentNumber
   }
+    let arrayCount = arrayOfNumbers.length
+    let mean = sumOfNumbers / arrayCount
+    let result = {
+        operation: "mean",
+        result: mean
+      }
+    
+      return res.send(result);
 })
 
-app.get('/median', (req, res, next) => {
+app.post('/median', (req, res, next) => {
+    let numbers = req.body['nums']
+    let arrayOfNumbers = numbers.sort((a, b)=> a-b);
+    let middleNum = Math.floor(arrayOfNumbers.length / 2)
+    let median;
+
+    return median = arrayOfNumbers % 2 === 0 ? arrayOfNumbers[middleNum]
 
 })
 
-app.get('/mode', (req, res, next) => {
+app.post('/mode', (req, res, next) => {
 
 })
 
 
-app.get('/all', (req, res, next){
-    try {
-
-    }
+app.post('/all', (req, res, next) => {
+   
 })
 
 // app.listen should always be at the bottom of your file
